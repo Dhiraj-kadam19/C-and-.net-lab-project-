33b1b6e3c39176905b91cfe042cffa2d4e040305
Dhiraj-kadam19 <dhirajkadam061@gmail.com>
2026-06-19 02:24:55 -0700

    Add files via upload

---
+++ ADDITION/Program.cs
@@ -0,0 +1,16 @@
+﻿using CLassLibrary;
+
+namespace ADDITION
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+
+             Class1 class1 = new Class1();
+           int result= class1.addition(4,3);
+            Console.WriteLine("print the addition "+result);
+        }
+    }
+}
---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace DelegatesDemo
+{
+    public delegate int Mydelegate(int P, int Q);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cmath m1 = new Cmath();
+            m1.multiply(10, 20);
+            Mydelegate add = new Mydelegate(m1.add);
+            int addresult = add(20, 10);
+            Console.WriteLine(addresult);
+            Mydelegate del = new Mydelegate(m1.subtract);
+            Console.WriteLine($"subtract={del(20, 10)}");
+
+        }
+    }
+
+    public class Cmath
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+        public int subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public int multiply(int x, int y)
+        {
+            return x * y;
+        }
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+
+}
