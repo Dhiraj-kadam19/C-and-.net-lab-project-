dc061407d5c80542bb561bc81d97e8d969d39f20
Dhiraj-kadam19 <dhirajkadam061@gmail.com>
2026-06-19 04:59:36 -0700

    Add files via upload

---
+++ CollectionDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+
+namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            st.Push(1);
+            st.Push(2);
+             st.Push(3);
+            st.Push(4);
+            int length=st.Count;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("print the stack member "+ st.Pop());
+            }
+
+            Queue q1=new Queue();
+            q1.Enqueue(1);
+            q1.Enqueue(2);
+            q1.Enqueue(3);
+            q1.Enqueue(12);
+            q1.Enqueue(13);
+            q1.Enqueue(14);
+            q1.Enqueue(15);
+            for (int j = 0; j <q1.Count; j++)
+            {
+                Console.WriteLine("print the queue value LIFO "+q1.Dequeue());
+            }
+        }
+
+    }
+}
